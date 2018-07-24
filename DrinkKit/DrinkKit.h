1693bffb45c54b235a6134f92c3135c9567a467f
James Ungaretti <19893438+jungaretti@users.noreply.github.com>
2018-07-24 17:14:25 -0500

    Create DrinkKit target in Xcode

---
+++ DrinkKit/DrinkKit.h
@@ -0,0 +1,17 @@
+//
+//  DrinkKit.h
+//  DrinkKit
+//
+//  Created by James Ungaretti on 7/24/18.
+//  Copyright © 2018 James Ungaretti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrinkKit.
+FOUNDATION_EXPORT double DrinkKitVersionNumber;
+
+//! Project version string for DrinkKit.
+FOUNDATION_EXPORT const unsigned char DrinkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrinkKit/PublicHeader.h>
