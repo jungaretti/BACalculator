4f58935e6d09dcb93a1e24ee6c68596f5825b1bf
James Ungaretti <19893438+jungaretti@users.noreply.github.com>
2018-07-26 21:05:19 -0500

    Create DrinkTableViewController to manage drink attribute tableView

---
+++ BACalculator/Controllers/DrinkTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  DrinkTableViewController.swift
+//  BACalculator
+//
+//  Created by James Ungaretti on 7/26/18.
+//  Copyright © 2018 James Ungaretti. All rights reserved.
+//
+
+import UIKit
+
+class DrinkTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "attribute") as! DrinkAttributeTableViewCell
+        switch indexPath.row {
+        case 0:
+            cell.attributeLabel.text = "Type"
+            cell.attributeButton.removeFromSuperview()
+        case 1:
+            cell.attributeLabel.text = "Size"
+            cell.attributeButton.setTitle("Use Custom Size", for: .normal)
+        default:
+            break
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200.0
+    }
+    
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
