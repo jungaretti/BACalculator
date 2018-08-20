71e134f5b3b384f5bf2738345cfdd29317808628
James Ungaretti <19893438+jungaretti@users.noreply.github.com>
2018-08-20 12:15:38 -0500

    Subclass HeaderView to make ActionHeaderView

---
+++ BACalculator/Views/Headers/ActionHeaderView.swift
@@ -0,0 +1,37 @@
+//
+//  ActionHeaderView.swift
+//  BACalculator
+//
+//  Created by James Ungaretti on 8/20/18.
+//  Copyright © 2018 James Ungaretti. All rights reserved.
+//
+
+import UIKit
+
+class ActionHeaderView: HeaderView {
+
+    var actionButton: UIButton = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        designActionButton()
+        layoutActionButton()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func designActionButton() {
+        actionButton.titleLabel?.textAlignment = .right
+        actionButton.titleLabel?.font = UIFont(name: "AvenirNext-Medium", size: 17.0)!
+    }
+    
+    private func layoutActionButton() {
+        addSubview(actionButton)
+        actionButton.translatesAutoresizingMaskIntoConstraints = false
+        actionButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16.0).isActive = true
+        actionButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+    }
+
+}
